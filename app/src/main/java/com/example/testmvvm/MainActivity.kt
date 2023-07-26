309f12379659a3ce1c18b7c72117296571ad9127
PCPerso <jeremie.neotic@free.fr>
2023-07-26 17:17:38 +0200

    Fenêtre Note

--- app/src/main/java/com/example/testmvvm/MainActivity.kt
+++ app/src/main/java/com/example/testmvvm/MainActivity.kt
@@ -1,21 +1,56 @@
 package com.example.testmvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
+    // Suite :
+    // Voir pourquoi l'item de la ZR prend toute la taille de la fenêtre'
+    // https://www.youtube.com/watch?v=QJUCD32dzHE
+
     private lateinit var oNoteViewModel : NoteViewModel
+
+    // Variable permettant de gérer la callback suite à la saisie d'une note
+    private val oStartForResultAjout = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when(result.resultCode){
+                RESULT_OK -> {
+                    var sTitreSaisi = result.data?.getStringExtra(NoteActivity.EXTRA_TITLE).toString()
+                    var sDescSaisi = result.data?.getStringExtra(NoteActivity.EXTRA_DESCRIPTION).toString()
+                    var nPrio = result.data?.getIntExtra(NoteActivity.EXTRA_PRIORITY,1)
+                    if (nPrio==null){
+                        nPrio = 0
+                    }
+
+                    var oNoteAjouter = Note(sTitle = sTitreSaisi, sDescription = sDescSaisi, nPriority = nPrio)
+                    oNoteViewModel.insert(oNoteAjouter)
+
+                    Toast.makeText(this,"Note saved",Toast.LENGTH_LONG).show()
+                }
+                RESULT_CANCELED -> {
+                    Toast.makeText(this,"Note cancelled",Toast.LENGTH_LONG).show()
+                } else -> {
+            } }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val varBTNAjout = findViewById<FloatingActionButton>(R.id.BTN_Add)
+        varBTNAjout.setOnClickListener {
+            val intent = Intent(this, NoteActivity::class.java)
+            oStartForResultAjout.launch(intent)
+        }
 
         // Déclaration du RecyclerView
         val varRecyclerView = findViewById<RecyclerView>(R.id.ZR_Notes)
--- app/src/main/java/com/example/testmvvm/NoteActivity.kt
+++ app/src/main/java/com/example/testmvvm/NoteActivity.kt
@@ -1,12 +1,23 @@
 package com.example.testmvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.NumberPicker
+import android.widget.Toast
 
 class NoteActivity : AppCompatActivity() {
+
+    // Passage paramètre à l'activity appelante
+    companion object{
+        const val EXTRA_TITLE = "com.example.testmvvm.TITLE"
+        const val EXTRA_DESCRIPTION = "com.example.testmvvm.EXTRA_DESCRIPTION"
+        const val EXTRA_PRIORITY = "com.example.testmvvm.EXTRA_PRIORITY"
+    }
 
     private lateinit var editTextTitle : EditText
     private lateinit var editTextDescription : EditText
@@ -27,10 +38,54 @@ class NoteActivity : AppCompatActivity() {
         this.title = "Add note"
 
 
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu_P: Menu?): Boolean {
         // https://www.youtube.com/watch?v=RhGMd8SsA14 => 11min50
-        return super.onCreateOptionsMenu(menu)
+        this.menuInflater.inflate(R.menu.add_note_menu,menu_P)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item_P: MenuItem): Boolean {
+
+        when (item_P.itemId) {
+
+            R.id.OPT_Save -> {
+                saveNote()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item_P)
+            }
+        }
+
+    }
+
+    private fun saveNote() {
+
+        val sTitle = editTextTitle.text.toString()
+        val sDescription = editTextDescription.text.toString()
+        val nPrio = numberPickerPriority.value
+
+        if (sTitle.trim()==""){
+            Toast.makeText(this, "Please insert a title",Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (sDescription.trim()==""){
+            Toast.makeText(this, "Please insert a description",Toast.LENGTH_LONG).show()
+            return
+        }
+
+        var oIntent = Intent()
+        oIntent.putExtra(EXTRA_TITLE,sTitle)
+        oIntent.putExtra(EXTRA_DESCRIPTION,sDescription)
+        oIntent.putExtra(EXTRA_PRIORITY,nPrio)
+
+        this.setResult(RESULT_OK,oIntent)
+
+        this.finish() // Ferme
+
     }
 }
